e8f6d16538ef076733e1907e5c18d38a546bc0fc
Gersho <zennoune.karim@outlook.fr>
2019-08-17 02:02:48 +0200

    first jsp vue

--- src/com/sdzee/servlets/Test.java
+++ src/com/sdzee/servlets/Test.java
@@ -1,8 +1,6 @@
 package com.sdzee.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,19 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 public class Test extends HttpServlet {
 
 	public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
-		response.setContentType("text/html");
-		response.setCharacterEncoding( "UTF-8" );
-		PrintWriter out = response.getWriter();
-		out.println("<!DOCTYPE html>");
-		out.println("<html>");
-		out.println("<head>");
-		out.println("<meta charset=\"utf-8\" />");
-		out.println("<title>Test</title>");
-		out.println("</head>");
-		out.println("<body>");
-		out.println("<p>Ceci est une page générée depuis une servlet.</p>");
-		out.println("</body>");
-		out.println("</html>");
+		this.getServletContext().getRequestDispatcher( "/WEB-INF/test.jsp" ).forward( request, response );
 		
 		
 		
